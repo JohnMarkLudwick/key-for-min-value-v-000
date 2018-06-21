d50103866562b2778587b0c663f5eb67be85f4db
JohnMark <johnmarkludwick@gmail.com>
2018-06-21 18:33:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+name_hash.each do |name, age|
+    if !name_hash[:age]
+      name_hash[:name] = 
+    end
 end
+
+
+
+
+
+
+
+
+
+
+#method needs to iterate and compare each age to collect the min age and return the #name of #that person. 
+
+
+
+
