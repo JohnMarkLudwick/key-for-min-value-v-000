96ec3e894c73ed5ccb3cfcb289e266963f947d9e
JohnMark <johnmarkludwick@gmail.com>
2018-06-21 18:46:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,11 @@
 
 def key_for_min_value(name_hash)
 
-name_hash.each do |name, age|
+  name_hash.each do |name, age|
     if !name_hash[:age]
       name_hash[:name] 
     end
-end
+  end
 
 
 end
