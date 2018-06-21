adf9616a737827f10e92925c24077dc694a355c8
JohnMark <johnmarkludwick@gmail.com>
2018-06-21 18:38:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,12 @@
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
-def key_for_min_value(name_hash)
-name_hash.each do |name, age|
-    if !name_hash[:age]
-      name_hash[:name] 
-    end
+# This didn't pass because it calls the "value" method
+def key_for_min_value_my_try(name_hash)
+  name_hash.key(name_hash.values.min)
 end
 
+key_for_min_value_my_try(ikea)
 
 
 
